06a87ffa10a19831805beac39bab4cbfbc3462a4
acillsadank <64443535+acillsadank@users.noreply.github.com>
2020-04-30 07:49:21 +0700

    Auto Install Debian 9 & 10

---
+++ about.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "Script Auto Install SSH & OpenVPN " | lolcat 
+echo -e ""
+echo -e "For Debian 9 32 bit & 64 bit" | lolcat
+echo -e "For VPS with KVM and VMWare virtualization" | lolcat
+echo -e ""
+echo -e "Original script by :" | lolcat
+echo -e "* Fornesia" | lolcat
+echo -e "* Rzengineer" | lolcat
+echo -e "* Fawzya" | lolcat
+echo -e ""
+echo -e "Mod by Rizwan Arif Firmansyah" | lolcat
+echo -e ""
+echo -e "   ██   ██  █████  ██████   █████  ██  " | lolcat
+echo -e "   ██  ██  ██   ██ ██   ██ ██   ██ ██          " | lolcat
+echo -e "   █████   ███████ ██   ██ ███████ ██           " | lolcat
+echo -e "   ██  ██   ██   ██ ██   ██ ██   ██ ██   " | lolcat
+echo -e "   ██   ██ ██   ██ ██████  ██   ██ ███████     " | lolcat
+echo -e ""
+echo -e " ███████  █████  ███    ███ ██ ██   ██    ██ " | lolcat
+echo -e " ██      ██   ██ ████  ████ ██ ██    ██  ██  " | lolcat
+echo -e " █████   ███████ ██ ████ ██ ██ ██     ████   " | lolcat
+echo -e " ██      ██   ██ ██  ██  ██ ██ ██      ██    " | lolcat
+echo -e " ██      ██   ██ ██      ██ ██ ███████ ██    " | lolcat
+echo -e "                                      The Big Kadall Family Grup™" | lolcat
+echo -e ""
+echo -e " No Wa 083145968517" | lolcat
